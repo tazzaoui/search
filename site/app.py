a976253a7ccee4044d90e7f5fe458ac809fc4722
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-09-04 00:07:25 -0400

    Updated site

--- site/app.py
+++ site/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+from ranker.main import main, file_to_link
+import time
+import os
 
 app = Flask(__name__)
 
@@ -10,5 +13,9 @@ def home():
 @app.route('/search/results', methods=['GET', 'POST'])
 def search_request():
     search_term = request.form["input"]
-    res = search_term
-    return render_template("results.html", res=res)
+    initial_time = time.time()
+    search_results = main("../indexer/index", search_term.lower())
+    res = [(file_to_link(os.path.join(b"../indexer/raw", path)), sim) for (path, sim) in search_results]
+    elapsed_time = time.time() - initial_time
+    num_results = len(search_results)
+    return render_template("results.html", res=res, elapsed_time=elapsed_time, num_results=num_results)
